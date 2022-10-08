a5bc94cf8c846f76d14fc719d1ac463ebfac2642
Nicholas Hammond <nhammond129@gmail.com>
2022-10-08 00:17:02 -0500

    alskdfjlaskdfjlskdfj

--- keyboards/sofle/keymaps/rgb_default/keymap.c
+++ keyboards/sofle/keymaps/rgb_default/keymap.c
@@ -1,5 +1,4 @@
-
- /* Copyright 2021 Dane Evans
+ /* Copyright 2022 Nicholas Hammond
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
@@ -14,45 +13,28 @@
   * You should have received a copy of the GNU General Public License
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
-  // SOFLE RGB
+
 #include <stdio.h>
 
 #include QMK_KEYBOARD_H
 
-#define INDICATOR_BRIGHTNESS 30
+struct {
+    uint8_t selector;
+    uint8_t selstep;
+    uint8_t rgb_mode;
+    uint8_t rgb_hue;
+    uint8_t rgb_sat;
+    uint8_t rgb_val;
+} gSTATE;
 
-#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
-#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
-
-// Light combinations
-#define SET_INDICATORS(hsv) \
-	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, hsv}
-#define SET_UNDERGLOW(hsv) \
-	{1, 6, hsv}, \
-    {35+1, 6,hsv}
-#define SET_NUMPAD(hsv)     \
-	{35+15, 5, hsv},\
-	  {35+22, 3, hsv},\
-	  {35+27, 3, hsv}
-#define SET_NUMROW(hsv) \
-	{10, 2, hsv}, \
-		{20, 2, hsv}, \
-		{30, 2, hsv}, \
-	  {35+ 10, 2, hsv}, \
-	  {35+ 20, 2, hsv}, \
-	  {35+ 30, 2, hsv}
-#define SET_INNER_COL(hsv)	\
-	{33, 4, hsv}, \
-	  {35+ 33, 4, hsv}
-
-#define SET_OUTER_COL(hsv) \
-	{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}
-#define SET_THUMB_CLUSTER(hsv) 	\
-	{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
-
+void clamp(uint8_t* var, const uint8_t min, const uint8_t max) {
+    if      (*var > max) { *var = max; }
+    else if (*var < min) { *var = min; }
+}
+void inc(uint8_t* var) { (*var)++; }
+void dec(uint8_t* var) { (*var)--; }
+void inc_step(uint8_t* var) { (*var) += gSTATE.selstep; }
+void dec_step(uint8_t* var) { (*var) -= gSTATE.selstep; }
 
 enum sofle_layers {
     _DEFAULTS = 0,
@@ -60,222 +42,232 @@ enum sofle_layers {
     _LOWER,
     _RAISE,
     _ADJUST,
+	_A_BASE
 };
 
 enum custom_keycodes {
-    KC_QWERTY = SAFE_RANGE,
-    KC_LOWER,
+    KC_LOWER = SAFE_RANGE,
     KC_RAISE,
     KC_ADJUST,
-    KC_D_MUTE
+	KC_RAISE2,
+	A_BASE_S, A_BASE_T, A_BASE_R, A_BASE_A,
+	A_BASE_O, A_BASE_I, A_BASE_Y, A_BASE_E
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT( \
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS, \
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_DELETE, \
-                 KC_LCTRL,KC_LGUI,KC_LALT, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, KC_RALT, KC_RGUI, KC_RCTRL \
+[_QWERTY] = LAYOUT( \
+  KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC, \
+  KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS, \
+  KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
+  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,     KC_B, KC_MUTE,   XXXXXXX, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_DELETE, \
+                   KC_LCTRL, KC_LGUI, KC_LALT, KC_LOWER,  KC_SPC,    KC_ENT, KC_RAISE, KC_RALT, KC_RGUI, KC_RCTRL \
 ),
 [_LOWER] = LAYOUT( \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,\
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,     KC_0,  KC_F12, \
-  KC_TAB,  _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
-  _______, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,      _______, KC_LBRC, KC_RBRC, _______, _______, KC_HOME, KC_END, \
-                     _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______\
+  KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11,\
+  KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,  KC_F12, \
+  KC_TAB,  _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, _______, _______, \
+  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,    _______, KC_LBRC, KC_RBRC, _______,  _______, KC_HOME,  KC_END, \
+                    _______, _______, _______, _______, KC_RAISE2,  _______, _______, _______, _______,  _______\
 ),
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                       _______, _______, KC_PSLS,  KC_PAST,  KC_MINUS,  KC_EQUAL, \
-  _______, _______, _______, _______, _______, _______,                       KC_PGUP, KC_7,    KC_8,     KC_9,     KC_PLUS,   KC_RBRC, \
-  _______, _______, _______, _______, _______, _______,                       KC_PGDN, KC_4,    KC_5,     KC_6,     _______,   KC_BSPC, \
-  _______, _______, _______, _______, _______, _______, _______,     _______, KC_0,    KC_1,    KC_2,     KC_3,     XXXXXXX,   _______, \
-                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______,                      _______, _______, KC_PSLS,  KC_PAST,  KC_MINUS,  KC_EQUAL, \
+  _______, _______, _______, _______, _______, _______,                      KC_PGUP, KC_7,    KC_8,     KC_9,     KC_PLUS,   KC_RBRC, \
+  _______, _______, _______, _______, _______, _______,                      KC_PGDN, KC_4,    KC_5,     KC_6,     _______,   KC_BSPC, \
+  _______, _______, _______, _______, _______, _______, _______,    _______, KC_0,    KC_1,    KC_2,     KC_3,     XXXXXXX,   _______, \
+                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______ \
 ),
-  [_ADJUST] = LAYOUT( \
-  XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  RESET   , XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, \
-  XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,   XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, \
-                   _______, _______, _______, _______,  _______,   _______, _______, _______, _______, _______ \
+[_ADJUST] = LAYOUT( \
+  XXXXXXX ,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  RESET   ,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX ,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, \
+  XXXXXXX ,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,   XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, \
+                    _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______ \
+),
+[_A_BASE]   = LAYOUT( \
+  _______, _______, _______, _______, _______,  _______,                      _______, _______, KC_PSLS,  KC_PAST,  KC_MINUS,  KC_EQUAL, \
+  _______, A_BASE_S,A_BASE_T,A_BASE_R,A_BASE_A, _______,                      KC_PGUP, KC_7,    KC_8,     KC_9,     KC_PLUS,   KC_RBRC, \
+  _______, A_BASE_O,A_BASE_I,A_BASE_Y,A_BASE_E, _______,                      KC_PGDN, KC_4,    KC_5,     KC_6,     _______,   KC_BSPC, \
+  _______, _______, _______, _______, _______,  _______, _______,    _______, KC_0,    KC_1,    KC_2,     KC_3,     XXXXXXX,   _______, \
+                    _______, _______, _______,  _______, _______,    _______, _______, _______, _______, _______ \
 ),
 };
 
 #ifdef RGBLIGHT_ENABLE
-char layer_state_str[70];
-// Now define the array of layers. Later layers take precedence
-
-// QWERTY,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS();
-
+const rgblight_segment_t PROGMEM rgblayer_lower[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 58, HSV_GREEN  } );
+const rgblight_segment_t PROGMEM rgblayer_raise[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 58, HSV_PURPLE } );
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    layer_qwerty_lights,
+	rgblayer_lower, rgblayer_raise
 );
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
-
-
-	rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
-	rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
-	rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
-    return state;
-}
 void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-	rgblight_mode(10);// haven't found a way to set this in a more useful way
+	rgblight_layers = my_rgb_layers;
+	gSTATE.rgb_mode = 7;
+	gSTATE.rgb_hue  = 136;
+	gSTATE.rgb_sat  = 255;
+	gSTATE.rgb_val  = 127;
+	gSTATE.selstep  = 1;
+
+	rgblight_mode(gSTATE.rgb_mode);
+
+    // toggle numlock on after init
+    if (!host_keyboard_led_state().num_lock) {
+        tap_code(KC_NUMLOCK);
+    }
+}
+layer_state_t layer_state_set_user(layer_state_t state) {
+	rgblight_set_layer_state(0, layer_state_cmp(state, _LOWER) && !layer_state_cmp(state, _ADJUST));
+	rgblight_set_layer_state(1, layer_state_cmp(state, _RAISE) && !layer_state_cmp(state, _ADJUST));
+	return state;
 }
 #endif
 
 #ifdef OLED_ENABLE
-
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
+void oled_printu8(uint8_t val) {
+	char buf[16];
+	sprintf(buf, "%3d\n", val);
+	oled_write(buf, false);
 }
 
-static void print_status_narrow(void) {
-    // Print current mode
-    oled_write_P(PSTR("\n\n"), false);
+static void right_oled(void) {
+	oled_write_ln_P(PSTR("moss"), false);
+}
+
+static void left_oled(void) {
+	static layer_state_t prevlayer;
+	layer_state_t toplayer = get_highest_layer(layer_state);
+	if (prevlayer != toplayer) {
+		oled_clear();
+		prevlayer = toplayer;
+	}
+
+    oled_write_P(PSTR("\n"), false);
     oled_write_ln_P(PSTR("Aura's\nKeeb"), false);
 
-    oled_write_ln_P(PSTR(""), false);
+	// Print current layer
+	oled_write_ln_P(PSTR("LAYER"), false);
+	switch (toplayer) {
+		case _QWERTY: oled_write_P(PSTR(" <3  "), false); break;
+		case _RAISE:  oled_write_P(PSTR("raise"), false); break;
+		case _LOWER:  oled_write_P(PSTR("lower"), false); break;
+		case _ADJUST: oled_write_P(PSTR("adjst"), false); break;
+		default: oled_write_P(PSTR("undef"), false); break;
+	}
 
-	//snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state)
+	if (toplayer == _ADJUST) {
+		oled_write_ln_P(PSTR("mode"), gSTATE.selector == 0);
+			oled_printu8(gSTATE.rgb_mode);
+		oled_write_ln_P(PSTR("hue") , gSTATE.selector == 1);
+			oled_printu8(gSTATE.rgb_hue);
+		oled_write_ln_P(PSTR("sat") , gSTATE.selector == 2);
+			oled_printu8(gSTATE.rgb_sat);
+		oled_write_ln_P(PSTR("val") , gSTATE.selector == 3);
+			oled_printu8(gSTATE.rgb_val);
+		oled_write_ln_P(PSTR("step"), gSTATE.selector == 4);
+			oled_printu8(gSTATE.selstep);
 
-
-    switch (get_highest_layer(default_layer_state)) {
-        case _QWERTY:
-            oled_write_ln_P(PSTR("Qwrt"), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR("Undef"), false);
-    }
-    oled_write_P(PSTR("\n\n"), false);
-    // Print current layer
-    oled_write_ln_P(PSTR("LAYER"), false);
-    switch (get_highest_layer(layer_state)) {
-        case _COLEMAK:
-        case _QWERTY:
-        case _COLEMAKDH:
-            oled_write_P(PSTR("Base\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("Raise"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower"), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("Adj\n"), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR("Undef"), false);
-    }
+	} else {
+		/* base layer */
+	}
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
+	return OLED_ROTATION_270;
+    // if (is_keyboard_master()) { }
+    // return rotation;
 }
 
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        print_status_narrow();
-    } else {
-        render_logo();
-    }
-    return false;
+    if   (is_keyboard_master()) { left_oled(); }
+    else { right_oled(); }
+	return false;
 }
 
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	bool keydown = record->event.pressed;
     switch (keycode) {
-        case KC_QWERTY:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return false;
-        case KC_LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            return false;
-        case KC_D_MUTE:
-            if (record->event.pressed) {
-                register_mods(mod_config(MOD_MEH));
-                register_code(KC_UP);
-            } else {
-                unregister_mods(mod_config(MOD_MEH));
-                unregister_code(KC_UP);
-            }
+        case KC_LOWER:  keydown ? layer_on(_LOWER)  : layer_off(_LOWER);	break;
+        case KC_RAISE:  keydown ? layer_on(_RAISE)  : layer_off(_RAISE);	break;
+        case KC_ADJUST: keydown ? layer_on(_ADJUST) : layer_off(_ADJUST);	break;
+		case KC_RAISE2: if (keydown) { layer_on(_A_BASE); break; }
+		default:        return true;
     }
-    return true;
+	update_tri_layer(_LOWER, _RAISE, _ADJUST);
+	return false;
 }
 
 #ifdef ENCODER_ENABLE
+void encoder_layered_none(uint8_t idx, bool cw) {
+	if (idx == 0) {			/* right */
+		if (cw) { tap_code(KC_UP); }
+		else	{ tap_code(KC_DOWN); }
+	} else if (idx == 1) {	/* left  */
+		if (cw) { tap_code(KC_VOLU); }
+		else	{ tap_code(KC_VOLD); }
+	}
+}
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if   (clockwise) { tap_code(KC_VOLU); }
-		else             { tap_code(KC_VOLD); }
-		} else if (index == 1) {
-			switch (get_highest_layer(layer_state)) {
-				case _QWERTY:
-					if (clockwise) {
-						tap_code(KC_PGDOWN);
-					} else {
-						tap_code(KC_PGUP);
-					}
-				break;
-			case _RAISE:
-			case _LOWER:
-					if (clockwise) {
-						tap_code(KC_DOWN);
-					} else {
-						tap_code(KC_UP);
-					}
-				break;
-			default:
-					if (clockwise) {
-						tap_code(KC_WH_D);
-					} else {
-						tap_code(KC_WH_U);
-					}
-				break;
+void encoder_layered_raise(uint8_t idx, bool cw) {
+	if (idx == 0) {			/* left */
+		if (cw) { tap_code(KC_VOLU); }
+		else	{ tap_code(KC_VOLD); }
+	} else if (idx == 1) {	/* right */
+		if (cw) { tap_code(KC_UP); }
+		else	{ tap_code(KC_DOWN); }
+	}
+}
+
+void encoder_layered_lower(uint8_t idx, bool cw) {
+	if (idx == 0) {			/* left */
+		if (cw) { tap_code(KC_VOLU); }
+		else	{ tap_code(KC_VOLD); }
+	} else if (idx == 1) {	/* right */
+		if (cw) { tap_code(KC_UP); }
+		else	{ tap_code(KC_DOWN); }
+	}
+}
+
+void encoder_layered_adjst(uint8_t idx, bool cw) {
+	if (idx == 0) {			/* left */
+		if (cw) { inc(&gSTATE.selector); }
+		else	{ dec(&gSTATE.selector); }
+		clamp(&gSTATE.selector, 0, 4);
+	} else if (idx == 1) {	/* right */
+		switch(gSTATE.selector) {
+			case 0: /* mode (hacky upper bound) */
+				if (cw) { inc(&gSTATE.rgb_mode); }
+				else	{ dec(&gSTATE.rgb_mode); }
+				clamp(&gSTATE.rgb_mode, 0, rgblight_get_mode()+1); break;
+			case 1: /* hue */
+				if (cw) { inc_step(&gSTATE.rgb_hue); }
+				else	{ dec_step(&gSTATE.rgb_hue); }
+				clamp(&gSTATE.rgb_hue, 0, 255); break;
+			case 2: /* sat */
+				if (cw) { inc_step(&gSTATE.rgb_sat); }
+				else	{ dec_step(&gSTATE.rgb_sat); }
+				clamp(&gSTATE.rgb_sat, 0, 255); break;
+			case 3: /* val (limited) */
+				if (cw) { inc_step(&gSTATE.rgb_val); }
+				else	{ dec_step(&gSTATE.rgb_val); }
+				clamp(&gSTATE.rgb_val, 0, 127); break;
+			case 4: /* step */
+				if (cw) { inc(&gSTATE.selstep); }
+				else	{ dec(&gSTATE.selstep); }
+				clamp(&gSTATE.selstep, 1, 16);  break;
 		}
+		rgblight_sethsv(gSTATE.rgb_hue, gSTATE.rgb_sat, gSTATE.rgb_val);
+		rgblight_mode(gSTATE.rgb_mode);
+	}
+	oled_task_user();
+}
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    clockwise = !clockwise;  // invert direction because of handswap
+    switch (get_highest_layer(layer_state)) {
+        case _RAISE:  encoder_layered_raise(index, clockwise); break;
+        case _LOWER:  encoder_layered_lower(index, clockwise); break;
+        case _ADJUST: encoder_layered_adjst(index, clockwise); break;
+        default:	  encoder_layered_none (index, clockwise); break;
     }
     return true;
 }
-
 #endif
